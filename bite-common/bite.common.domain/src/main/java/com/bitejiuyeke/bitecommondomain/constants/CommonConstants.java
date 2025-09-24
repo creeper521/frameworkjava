26bbdd096cf14fb08810c6d1a1c2ed918384fc20
crjs-hao <19603710129@163.com>
2025-09-24 17:38:26 +0800

    up

---
+++ bite-common/bite.common.domain/src/main/java/com/bitejiuyeke/bitecommondomain/constants/CommonConstants.java
@@ -0,0 +1,26 @@
+package com.bitejiuyeke.bitecommondomain.constants;
+
+/**
+ * 通用常量
+ */
+public class CommonConstants {
+    /**
+     * 通用日期格式
+     */
+    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 默认编码
+     */
+    public static final String UTF8 = "UTF-8";
+
+    /**
+     * 默认分隔符
+     */
+    public static final String DEFAULT_DELIMITER = ",";
+
+    /**
+     * 空字符串
+     */
+    public static final String EMPTY_STR = "";
+}
