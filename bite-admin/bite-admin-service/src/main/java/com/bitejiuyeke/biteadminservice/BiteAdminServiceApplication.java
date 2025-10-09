e127843735c1c236918a7027b3904aa7fc359d53
crjs-hao <19603710129@163.com>
2025-10-09 17:48:22 +0800

    up

---
+++ bite-admin/bite-admin-service/src/main/java/com/bitejiuyeke/biteadminservice/BiteAdminServiceApplication.java
@@ -0,0 +1,28 @@
+package com.bitejiuyeke.biteadminservice;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@MapperScan("com.bitejiuyeke.**.mapper")
+@SpringBootApplication
+@EnableScheduling
+public class BiteAdminServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BiteAdminServiceApplication.class, args);
+        log.info("基础管理服务启动成功");
+    }
+    /**
+     * 分页拦截器
+     * @return mapper分页查询
+     */
+    @Bean
+    public PaginationInnerInterceptor paginationInterceptor(){
+        return new PaginationInnerInterceptor();
+    }
+}
